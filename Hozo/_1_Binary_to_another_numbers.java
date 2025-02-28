89fa318976d48751541aadf5c0a1055511c1f34a
AbdFaris513 <106486197+AbdFaris513@users.noreply.github.com>
2025-02-28 09:07:01 +0530

    x

---
+++ Hozo/_1_Binary_to_another_numbers.java
@@ -0,0 +1,12 @@
+package Hozo;
+
+public class _1_Binary_to_another_numbers {
+    public static void main(String[] args) {
+        String sbn = "1100100";
+        int dbn = 1100100;
+        int bn = 0b1100100;
+
+
+    }
+
+}
